f6f66c4923caef48323f1bc51739cd5b5337b82e
Gordo <gordonhdo@gmail.com>
2022-04-24 19:10:01 -0700

    second attempt

--- index.js
+++ index.js
@@ -1,228 +1,213 @@
 // Node Modules
 const inquirer = require('inquirer');
+const path = require('path');
 const fs = require('fs');
-const markdown = require('./src/generateHTML');
+const fse = require('fs-extra');
 
-// Subclasses of team profile
-const Manager = require('./lib/Manager');
+// Import Subclasses
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
 
-// Create an array to store team members
+// Create Team Array
 const teamProfile = [];
 
-// Function initializes the app
+// HTML generator
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "teamProfile.html")
+const renderHTML = require('./src/generateHTML');
+
+// Init function
 function init() {
-    managerPrompt();
+  addManager();
 }
 
-// Prompt user for Manager's information on startup
-function managerPrompt() {
-    return inquirer.prompt ([
-        {
-            type: 'input',
-            name: 'managerName',
-            message: "Start by entering your team manager's name: ",
-            // boolean checks for valid entry
-            validate: input => {
-                if (input) {
-                    return true;
-                } else {
-                    console.log('*Manager name is required!*')
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'managerId',
-            message: "Please enter team manager's ID: ",
-            // boolean checks if member ID is entered as a valid number
-            validate: input => {
-                if (isNaN(input)) {
-                    console.error('*That is not a valid ID!*')
-                } else {
-                    return true;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'managerEmail',
-            message: "Please enter your manager's email: ",
-            // using validate command and boolean to check if valid email is entered
-            validate: managerEmail => {
-                return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(managerEmail)
-            }
-        },
-        {
-            type: 'input',
-            name: 'managerOfficeNumber',
-            message: "Please enter your Manager's office number: ",
-            // boolean checks if office number is a valid number
-            validate: officeNumber => {
-                if (isNaN(officeNumber)) {
-                    console.log('*That is not a valid office number!*')
-                    return false;
-                } else {
-                    return true;
-                }
-            }
+// Inquirer Prompt for Manager Data
+function addManager() {
+  inquirer.prompt([
+    {
+      type: "input",
+      message: "What is the name of your team Manager?",
+      name: "managerName",
+      validate: managerName => {
+        if (managerName === '') {
+          console.log('*Manager Name Required!')
+          return false;
+        } else {
+          return true;
         }
-    ])
-    // stores manager information to team array and opens main menu
-    .then(input => { 
-        const manager = new Manager (input.managerName, input.managerId, input.managerEmail, input.managerOfficeNumber);
-        teamProfile.push(manager);
-        console.log('Manager successfully added to the team!');
-        toMenu();
-    })
-};
+      }
+    },
+    {
+      type: "input",
+      message: "What is your Manager's ID number?",
+      name: "managerId",
+    },
+    {
+      type: "input",
+      message: "What is your Manager's email address?",
+      name: "managerEmail",
+      validate: managerEmail => {
+        if (managerEmail) {
+          return true;
+        } else {
+          console.log('Please enter a valid email address');
+        }
+      }
+    },
+    {
+      type: "input",
+      message: "What is your Manager's office number?: ",
+      name: "managerOfficeNumber",
+      validate: managerOfficeNumber => {
+        if (isNaN(managerOfficeNumber)) {
+          console.log('*Please enter a valid number*')
+          return false;
+        } else {
+          return true;
+        }
+      }
+    },
+  ])
+  // Push New Manager data, display answers, show team array
+  .then(answers => {
+    console.log("Your Manager has been added!", answers)
+    let newManager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber)
+    teamProfile.push(newManager)
+    console.log(teamProfile)
+    menu();
+  })
+}
 
-// Main Menu
-function toMenu() {
-    return inquirer.prompt ([
-        {
-            type: 'list',
-            name: 'menu',
-            message: "What would you like to do next?",
-            choices: ["Add another team member", "Finish"]
+// Inquirer Prompt for Engineer Data
+function addEngineer() {
+  inquirer.prompt([
+    {
+      type: "input",
+      message: "Please enter Engineer's name: ",
+      name: "engineerName",
+      validate: engineerName => {
+        if (engineerName === "") {
+          console.log("*Please Enter Your Engineer's Name*")
+          return false;
+        } else {
+          return true;
         }
-    ]) .then (answers => {
-        if (answers.menu === "Add another team member") {
-            addTeamMember();
-        } else if (answers.menu === "Finish") {
-            console.log('Team Profile has been generated')
-            return teamProfile;
+      }
+    },
+    {
+      type: "input",
+      message:"What is your Engineer's Id number?: ",
+      name: "engineerId",
+    },
+    {
+      type: "input",
+      message: "Please enter Engineer's email address: ",
+      name: "engineerEmail",
+      validate: engineerEmail => {
+        if (engineerEmail) {
+          return true;
+        } else {
+          console.log('Please enter a valid email address');
         }
-    })
-};
+      }
+    },
+    {
+      type: "input",
+      message: "Please enter your Engineer's github username: ",
+      name: "engineerGithub",
+    },
+  ])
+  // Push New Engineer data, display answers, show team array
+  .then(answers => {
+    console.log("Your Engineer has been added!", answers)
+    let newEngineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGithub)
+    teamProfile.push(newEngineer)
+    console.log(teamProfile)
+    menu();
+  })
+}
 
-// Function adds team members by role selection:
-function addTeamMember() {
-    return inquirer.prompt([
-        {
-            type: 'list',
-            name: 'role',
-            message: "Who would you like to add? ",
-            choices: ['Engineer', 'Intern', 'Manager',]
+// Inquirer Prompt for Intern Data
+function addIntern() {
+  inquirer.prompt([
+    {
+      type: "input",
+      message: "What is your Intern's name?: ",
+      name: "internName",
+    },
+    {
+      type: "input",
+      message: "What is your Intern's Id number?: ",
+      name: "internId",
+    },
+    {
+      type: "input",
+      message: "What is your Intern's email address?: ",
+      name: "internEmail",
+      validate: internEmail => {
+        if (internEmail) {
+          return true;
+        } else {
+          console.log('Please enter a valid email address');
         }
-    ]) .then (input => {
-        if (input.role === 'Manager') {
-            managerPrompt();
-        } else if (input.role === 'Engineer') {
-            engineerPrompt();
-        } else if (input.role === 'Intern') {
-            internPrompt();
-        }
-    })
-};
+      }
+    },
+    {
+      type: "input",
+      message: "Where does your Intern go to school?: ",
+      name: "internSchool",
+    },
+  ])
+  // Push New Intern data, display answers, show team array
+  .then(answers => {
+    console.log("Your Intern has been added!", answers)
+    let newIntern = new Intern(answers.internName, answers.internId, answers.internEmail, answers.internSchool)
+    teamProfile.push(newIntern)
+    console.log(teamProfile)
+    menu();
+  })
+}
 
-// Prompt user for Engineer information
-function engineerPrompt() {
-    return inquirer.prompt ([
-        {
-            type: 'input',
-            name: 'engineerName',
-            message: "Please enter Engineer's name: ",
-            validate: input => {
-                if (input) {
-                    return true;
-                } else {
-                    console.log("*Team Member name required!*")
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'engineerId',
-            message: "Please enter Engineer's ID: ",
-            validate: input => {
-                if (isNaN(input)) {
-                    console.log('*That is not a valid ID!*')
-                    return false;
-                } else {
-                    return true;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'engineerEmail',
-            message: "Please enter Engineer's email: ",
-            validate: engineerEmail => {
-                return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(engineerEmail)
-            }
-        },
-        {
-            type: 'input',
-            name: 'engineerGithub',
-            message: "Please enter Engineer's GitHub username: ",
-        }
-    ]) // stores engineer information to team array
-    .then (input => { 
-        const engineer = new Engineer (input.engineerName, input.engineerId, input.engineerEmail, input.engineerGithub);
-        teamProfile.push(engineer);
-        console.log('Engineer successfully added to the team!' + engineer);
-        toMenu();
-    })
-};
+// Inquirer Prompt for Main Menu
+function menu() {
+  inquirer.prompt([
+    {
+      type: 'list',
+      name: 'menu',
+      message: "Who would you like to add?: ",
+      // add menu options
+      choices: [
+        "Add an Engineer", 
+        "Add an Intern", 
+        "Finish",
+      ],
+    },
+  ])
+  .then(choices => { // redirect
+    if (choices.menu === "Add an Engineer") {
+      addEngineer();
+    } else if (choices.menu === "Add an Intern") {
+      addIntern();
+    } else if (choices.menu === "Finish") {
+      console.log("Your team profile has been created!", teamProfile);
+      finish();
+    }
+  })
+}
 
-function internPrompt() {
-    return inquirer.prompt ([
-        {
-            type: 'input',
-            name: 'internName',
-            message: "Please enter Intern's name: ",
-            validate: input => {
-                if (input) {
-                    return true;
-                } else {
-                    console.log("*Team Member name required!*")
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'internId',
-            message: "Please enter Intern's ID: ",
-            validate: input => {
-                if (isNaN(input)) {
-                    console.log('*That is not a valid ID!*')
-                    return false;
-                } else {
-                    return true;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'internEmail',
-            message: "Please enter Intern's email: ",
-            validate: internEmail => {
-                return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(internEmail)
-            }
-        },
-        {
-            type: 'input',
-            name: 'internSchool',
-            message: "Please enter Intern's school: ",
-        }
-    ]) // stores intern information to team array
-    .then (input => { 
-        const intern = new Intern (input.internName, input.internId, input.internEmail, input.internSchool);
-        teamProfile.push(intern);
-        console.log(intern + 'Intern successfully added to the team!');
-        toMenu();
-    })
-};
+const finish = () => {
+  let teamRender = renderHTML(teamProfile)
+  fse.outputFile('dist/team-profile.html', teamRender)
+  .then(() => {
+    console.log('Your team profile has been successfully rendered');
+  })
+  .catch(err => {
+    console.log(err)
+  });
+}
 
 
+
+// Call Init
 init();
-
-
-// External Links:
-// Email validation using Inquirer: https://gist.github.com/Amitabh-K/ae073eea3d5207efaddffde19b1618e8
-// Inquirer Checkbox: https://www.digitalocean.com/community/tutorials/nodejs-interactive-command-line-prompts
